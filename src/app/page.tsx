aae877d0662653d352f43e62517d9909f88f5345
Vasil Genov <vasil.genov@psinteractive.io>
2024-01-31 18:32:35 +0200

    feat: tailwind setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      next-blog
+    <main className="mx-auto px-6">
+      <p className="mb-12 mt-12 text-center text-3xl dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Dave</span>.
+        </span>
+      </p>
     </main>
   )
 }
